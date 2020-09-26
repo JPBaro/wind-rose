b9c3da84e1e703a47ebd8b210ace0659652e93b8
JPBaro <joaquinpb.tech@gmail.com>
2020-09-26 17:32:15 +0200

    remove default generated code

--- projects/FirstWebComponente/src/app/app.module.ts
+++ projects/FirstWebComponente/src/app/app.module.ts
@@ -1,28 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, DoBootstrap, Injector } from '@angular/core';
 import { AppComponent } from './app.component';
-import { UIButtonComponent } from './uibutton/uibutton.component';
 import { createCustomElement } from '@angular/elements';
 import { WindRosetaComponent } from './wind-roseta/wind-roseta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UIButtonComponent,
     WindRosetaComponent
   ],
   imports: [
     BrowserModule
   ],
-  entryComponents: [UIButtonComponent, WindRosetaComponent],
+  entryComponents: [ WindRosetaComponent],
   bootstrap: [AppComponent]
 })
 
 export class AppModule implements DoBootstrap{
 
   constructor(private injector: Injector) {
-    const webComponent = createCustomElement(UIButtonComponent, {injector});
-    customElements.define('ui-button', webComponent);
 
     const webComponentWind = createCustomElement(WindRosetaComponent, {injector});
     customElements.define('wind-rosete', webComponentWind);
